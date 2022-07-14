2b06e4fad381f92843e8faea180bf8590031fd8c
Ongkay <bisnismilyarandollar@gmail.comm>
2022-07-14 17:49:31 +0700

    update apps

--- app.js
+++ app.js
@@ -5,18 +5,19 @@ const logger = require('morgan');
 
 const app = express();
 
-// import router categories
+// router
 const categoriesRouter = require('./app/api/v1/categories/router');
 const imagesRouter = require('./app/api/v1/images/router');
 const talentsRouter = require('./app/api/v1/talents/router');
 const eventsRouter = require('./app/api/v1/events/router');
-
-// membuat variabel v1
-const v1 = '/api/v1/cms';
+const organizersRouter = require('./app/api/v1/organizers/router');
+const authCMSRouter = require('./app/api/v1/auth/router');
 
 // middlewares
 const notFoundMiddleware = require('./app/middlewares/not-found');
 const handleErrorMiddleware = require('./app/middlewares/handler-error');
+
+const v1 = '/api/v1/cms';
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -30,11 +31,13 @@ app.get('/', (req, res) => {
   });
 });
 
-// gunakan categories router
 app.use(v1, categoriesRouter);
 app.use(v1, imagesRouter);
 app.use(v1, talentsRouter);
 app.use(v1, eventsRouter);
+app.use(v1, organizersRouter);
+app.use(v1, organizersRouter);
+app.use(v1, authCMSRouter);
 
 // middlewares
 app.use(notFoundMiddleware);
